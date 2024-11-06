961028ef3de3101a5690faadef832605e0a08d1a
MhdYoussef <mohammad.youssef963@gmail.com>
2024-11-06 15:23:55 +0300

    Add Brand Type Commit

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -11,5 +11,7 @@ namespace Core.Interfaces
     {
         Task<Product> GetProductByIdAsync(int id);
         Task<IReadOnlyList<Product>> GetProductAsync();
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
     }
 }
